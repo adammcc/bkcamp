a50c970881bdb4cbe150a6de24176bc62421a6a6
adammcc <ajm845@gmail.com>
2016-09-13 21:49:09 -0400

    Add articles_controller
    Bonus: Remove some useless code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,35 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def index
-    @location_path = "/#{params[:path]}"
-  end
-
-  def get_data
-    home_images = HomeImage.all
-    home_images_json = home_images.map { |hi| [hi.src, hi.title, hi.path] }
-
-    articles = Article.all
-    article_info_json = {}
-    articles.each do |article|
-      article_info_json[article.alt.to_s] = {
-        src: article.src,
-        alt: article.alt,
-        title: article.title,
-        article_type: article.article_type,
-        filter: article.filter,
-        feature_article: article.feature_article,
-        feature_img_src: article.feature_img_src,
-        main_blurb: article.main_blurb,
-        blurb: article.blurb,
-        instructions: article.instructions,
-        recipe: article.recipe,
-        pics: article.pics,
-        home_image: article.home_image
-      }
-    end
-
-    render :json => { "home_images" => home_images_json, "info" => article_info_json }
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'application#index'
-  get "/" => "application#index"
-  get "/data" => "application#get_data"
+
+  resources :articles, only: [:index]
 end
