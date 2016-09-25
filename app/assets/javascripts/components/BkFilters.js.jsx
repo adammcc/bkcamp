89c156d3cc57e56d5fe4e39244fede9bb2ca401a
adammcc <ajm845@gmail.com>
2016-09-25 10:45:27 -0400

    Combine make and finds into lifestyle

--- app/assets/javascripts/components/BkFilters.js.jsx
+++ app/assets/javascripts/components/BkFilters.js.jsx
@@ -12,30 +12,25 @@ var BkFilters = React.createClass({
   render: function () {
     return (
       <div className="bk-filters__row row">
-        <div className="bk-filters__container col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0">
+        <div className="bk-filters__container col-sm-10 col-sm-offset-1 col-xs-12 col-xs-offset-0">
           <Link to='/articles/everything'>
-            <div onClick={ this.setFilter.bind(null,'everything') } className={ (this.state.filterBy == 'everything') ? "col-sm-2 col-xs-2 col-sm-offset-1 col-xs-offset-1 bk-filters__option -selected" : "col-sm-2 col-xs-2 col-sm-offset-1 col-xs-offset-1 bk-filters__option" }>
+            <div onClick={ this.setFilter.bind(null,'everything') } className={ (this.state.filterBy == 'everything') ? "col-sm-3 col-xs-3 bk-filters__option -selected" : "col-sm-3 col-xs-3 bk-filters__option" }>
               All
             </div>
           </Link>
           <Link to='/articles/getayways'>
-            <div onClick={ this.setFilter.bind(null,'getayways') } className={ this.state.filterBy == 'getayways' ? "col-sm-2 col-xs-2 bk-filters__option -selected" : "col-sm-2 col-xs-2 bk-filters__option" }>
+            <div onClick={ this.setFilter.bind(null,'getayways') } className={ this.state.filterBy == 'getayways' ? "col-sm-3 col-xs-3 bk-filters__option -selected" : "col-sm-3 col-xs-3 bk-filters__option" }>
               Near
             </div>
           </Link>
           <Link to='/articles/treks'>
-            <div onClick={ this.setFilter.bind(null,'treks') } className={ this.state.filterBy == 'treks' ? "col-sm-2 col-xs-2 bk-filters__option -selected" : "col-sm-2 col-xs-2 bk-filters__option" }>
+            <div onClick={ this.setFilter.bind(null,'treks') } className={ this.state.filterBy == 'treks' ? "col-sm-3 col-xs-3 bk-filters__option -selected" : "col-sm-3 col-xs-3 bk-filters__option" }>
               Far
             </div>
           </Link>
-          <Link to='/articles/things'>
-            <div onClick={ this.setFilter.bind(null,'things') } className={ this.state.filterBy == 'things' ? "col-sm-2 col-xs-2 bk-filters__option -selected" : "col-sm-2 col-xs-2 bk-filters__option" }>
-              Finds
-            </div>
-          </Link>
-          <Link to='/articles/recipes'>
-            <div onClick={ this.setFilter.bind(null,'recipes') } className={ this.state.filterBy == 'recipes' ? "col-sm-2 col-xs-2 bk-filters__option -selected" : "col-sm-2 col-xs-2 bk-filters__option" }>
-              Make
+          <Link to='/articles/lifestyle'>
+            <div onClick={ this.setFilter.bind(null,'lifestyle') } className={ this.state.filterBy == 'lifestyle' ? "col-sm-3 col-xs-3 bk-filters__option -selected" : "col-sm-3 col-xs-3 bk-filters__option" }>
+              Lifestyle
             </div>
           </Link>
         </div>
